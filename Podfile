dae2477a513fc56f66a9ece9dffa0cd56a2d2a6a
akuraru <akuraru@gmail.com>
2020-01-08 22:37:31 +0900

    bundle exec pod install SnapKit and RxCocoa

--- Podfile
+++ Podfile
@@ -7,6 +7,9 @@ target 'morion' do
   pod 'Firebase/Analytics'
   pod 'Firebase/Database'
 
+  pod 'SnapKit'
+  pod 'RxCocoa'
+
   target 'morionTests' do
     inherit! :search_paths
 
--- Pods/Target Support Files/Pods-morion/Pods-morion-frameworks.sh
+++ Pods/Target Support Files/Pods-morion/Pods-morion-frameworks.sh
@@ -163,14 +163,18 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RIBs/RIBs.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RIBs/RIBs.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
